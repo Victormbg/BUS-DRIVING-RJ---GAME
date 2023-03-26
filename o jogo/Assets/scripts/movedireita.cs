000c122dc40e8f6065b4163389c88b125eb917c8
Victor Manuel de Barros Garcia <victormbg2000@gmail.com>
2023-03-26 17:08:43 -0300

    Melhorias nos arquivos do projeto

---
+++ o jogo/Assets/scripts/movedireita.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movedireita : MonoBehaviour
+{
+    public float vel;
+
+    void Update()
+    {
+        transform.Translate(vel * Time.deltaTime, 0, 0);
+    }
+}
